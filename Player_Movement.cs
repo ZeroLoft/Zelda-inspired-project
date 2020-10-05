65fdbafe9687f9902d0c6e236d45f19dbd05cd9e
Ali-Muhsin <72403232+Ali-Muhsin@users.noreply.github.com>
2020-10-05 21:42:08 +0530

    Add files via upload

---
+++ Player_Movement.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player_Movement : MonoBehaviour
+{
+    public float speed;
+    private Rigidbody2D myRigidbody;
+    private Vector3 change;
+    // Start is called before the first frame update
+    void Start()
+    {
+
+        myRigidbody = GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        change = Vector3.zero;
+        //change.x = Input.GetAxisRaw("Horizontal");
+        // change.y = Input.GetAxisRaw("Vertical");
+        change.x = Input.GetAxisRaw("Horizontal") * Time.deltaTime * speed;
+        change.y = Input.GetAxisRaw("Vertical") * Time.deltaTime * speed;
+        if (change != Vector3.zero)
+        {
+            transform.Translate(new Vector3(change.x, change.y));
+        }
+        MoveCharacter();
+        
+    //if (change != Vector3.zero)
+    // {        {
+    //   MoveCharacter();
+    //}
+
+}
+    void MoveCharacter()
+    {
+        myRigidbody.MovePosition(
+            transform.position + change * speed * Time.deltaTime
+    );
+
+}
+}
+
